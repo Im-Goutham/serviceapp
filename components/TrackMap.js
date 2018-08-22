4a36a932783f4d5e6423bf77dab951bceeef25f8
Goutham <gouthamrs4u@gmail.com>
2018-08-22 16:40:00 +0530

    Track screen completed

--- components/TrackMap.js
+++ components/TrackMap.js
@@ -1,157 +1,88 @@
-import React from "react";
+import React, { Component } from 'react';
 import {
   StyleSheet,
-  View,
   Text,
-  TouchableOpacity,
-  Platform
-} from "react-native";
-import MapView, { Marker, AnimatedRegion, Polyline } from "react-native-maps";
-import haversine from "haversine";
+  View,
+  Dimensions
+} from 'react-native';
 
-const LATITUDE = 29.95539;
-const LONGITUDE = 78.07513;
-const LATITUDE_DELTA = 0.009;
-const LONGITUDE_DELTA = 0.009;
+import MapView ,{ Marker } from 'react-native-maps';
+import Polyline from '@mapbox/polyline';
 
-class AnimatedMarkers extends React.Component {
+export default class RnDirectionsApp extends Component {
   constructor(props) {
-    super(props);
-
+    super(props)
     this.state = {
-      latitude: LATITUDE,
-      longitude: LONGITUDE,
-      routeCoordinates: [],
-      distanceTravelled: 0,
-      prevLatLng: {},
-      coordinate: new AnimatedRegion({
-        latitude: LATITUDE,
-        longitude: LONGITUDE
-      })
-    };
-  }
-
-  componentWillMount() {
-    navigator.geolocation.getCurrentPosition(
-      position => {},
-      error => alert(error.message),
-      {
-        enableHighAccuracy: true,
-        timeout: 20000,
-        maximumAge: 1000
-      }
-    );
+      coords: [],
+      markers: [
+        {latitude: 23.022505, longitude: 72.571365},
+        {latitude: 23.032469, longitude: 72.491236}
+      ],
+    }
   }
 
   componentDidMount() {
-    this.watchID = navigator.geolocation.watchPosition(
-      position => {
-        const { coordinate, routeCoordinates, distanceTravelled } = this.state;
-        const { latitude, longitude } = position.coords;
-
-        const newCoordinate = {
-          latitude,
-          longitude
-        };
-
-        if (Platform.OS === "android") {
-          if (this.marker) {
-            this.marker._component.animateMarkerToCoordinate(
-              newCoordinate,
-              500
-            );
-          }
-        } else {
-          coordinate.timing(newCoordinate).start();
-        }
-
-        this.setState({
-          latitude,
-          longitude,
-          routeCoordinates: routeCoordinates.concat([newCoordinate]),
-          distanceTravelled:
-            distanceTravelled + this.calcDistance(newCoordinate),
-          prevLatLng: newCoordinate
-        });
-      },
-      error => console.log(error),
-      { enableHighAccuracy: true, timeout: 20000, maximumAge: 1000 }
-    );
+    // find your origin and destination point coordinates and pass it to our method.
+    // I am using Bursa,TR -> Istanbul,TR for this example
+    this.getDirections("23.022505, 72.571365", "23.032469,72.491236")
   }
 
-  calcDistance = newLatLng => {
-    const { prevLatLng } = this.state;
-    return haversine(prevLatLng, newLatLng) || 0;
-  };
-
-  getMapRegion = () => ({
-    latitude: this.state.latitude,
-    longitude: this.state.longitude,
-    latitudeDelta: LATITUDE_DELTA,
-    longitudeDelta: LONGITUDE_DELTA
-  });
+  async getDirections(startLoc, destinationLoc) {
+        try {
+            let resp = await fetch(`https://maps.googleapis.com/maps/api/directions/json?origin=${ startLoc }&destination=${ destinationLoc }`)
+            let respJson = await resp.json();
+            let points = Polyline.decode(respJson.routes[0].overview_polyline.points);
+            let coords = points.map((point, index) => {
+                return  {
+                    latitude : point[0],
+                    longitude : point[1]
+                }
+            })
+            this.setState({coords: coords})
+            return coords
+        } catch(error) {
+            alert(error)
+            return error
+        }
+    }
 
   render() {
-
     return (
-      <View style={styles.container}>
-        <MapView
-          style={styles.map}
-          showUserLocation
-          followUserLocation
-          loadingEnabled
-          region={this.getMapRegion()}
-        >
-          <Polyline strokeColor='red' lineJoin='bevel' coordinates={this.state.routeCoordinates} strokeWidth={5} />
-          <Marker.Animated 
-            ref={marker => {
-              this.marker = marker;
-            }}
-            coordinate={this.state.coordinate} />
+      <View>
+        <MapView style={styles.map} initialRegion={{
+          latitude:23.022505, 
+          longitude:72.571365, 
+          latitudeDelta: 0.0922,
+          longitudeDelta: 0.0421
+        }}>
+
+        <MapView.Polyline 
+            coordinates={this.state.coords}
+            strokeWidth={4}
+            strokeColor="#ff9933"/>
+  {
+         this.state.markers.map((coordinate,key)=>{
+             return   <Marker
+             coordinate={coordinate}
+             key={key}
+           >
+           </Marker>
+         })
+      } 
         </MapView>
-        <View style={styles.buttonContainer}>
-          <TouchableOpacity style={[styles.bubble, styles.button]}>
-            <Text style={styles.bottomBarContent}>
-              {parseFloat(this.state.distanceTravelled).toFixed(2)} km
-            </Text>
-          </TouchableOpacity>
-        </View>
       </View>
     );
   }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    ...StyleSheet.absoluteFillObject,
-    justifyContent: "flex-end",
-    alignItems: "center"
-  },
   map: {
-    ...StyleSheet.absoluteFillObject
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0,
+    width: Dimensions.get('window').width,
+    height: Dimensions.get('window').height
   },
-  bubble: {
-    flex: 1,
-    backgroundColor: "rgba(255,255,255,0.7)",
-    paddingHorizontal: 18,
-    paddingVertical: 12,
-    borderRadius: 20
-  },
-  latlng: {
-    width: 200,
-    alignItems: "stretch"
-  },
-  button: {
-    width: 80,
-    paddingHorizontal: 12,
-    alignItems: "center",
-    marginHorizontal: 10
-  },
-  buttonContainer: {
-    flexDirection: "row",
-    marginVertical: 20,
-    backgroundColor: "transparent"
-  }
 });
-
-export default AnimatedMarkers;
--- screens/JobTrackScreen.js
+++ screens/JobTrackScreen.js
@@ -45,7 +45,6 @@ const styles = StyleSheet.create({
       container: {
           flex:1,
           padding:5,
-          paddingBottom:40,
           backgroundColor:'white'
     }
 })
