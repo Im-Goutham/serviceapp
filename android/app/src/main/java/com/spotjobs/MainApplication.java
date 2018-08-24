48724af5cc353424904ffa93b7c4fa922702e882
Goutham <gouthamrs4u@gmail.com>
2018-08-24 15:32:02 +0530

    Splashscreen added for ios

--- android/app/src/main/java/com/spotjobs/MainApplication.java
+++ android/app/src/main/java/com/spotjobs/MainApplication.java
@@ -7,6 +7,7 @@ import com.facebook.appevents.AppEventsLogger;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.oblador.vectoricons.VectorIconsPackage;
 import org.devio.rn.splashscreen.SplashScreenReactPackage;
 import com.zmxv.RNSound.RNSoundPackage;
 import com.rnim.rn.audio.ReactNativeAudioPackage;
@@ -47,6 +48,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new VectorIconsPackage(),
             new SplashScreenReactPackage(),
             new RNSoundPackage(),
             new ReactNativeAudioPackage(),
