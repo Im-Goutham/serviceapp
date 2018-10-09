17af51cff6a71b1800965976ca189dcf92602489
abhishek kalia <kabhi1792@gmail.com>
2018-10-09 19:23:58 +0530

    function testing or state testing initilize

--- screens/__tests__/AppTutorial.test.js
+++ screens/__tests__/AppTutorial.test.js
@@ -1,28 +1,19 @@
 jest.unmock('redux-mock-store')
 jest.unmock('redux-thunk')
 import React from 'react';
+import {TouchableOpacity} from 'react-native';
 import { shallow, mount, render } from 'enzyme';
 import configureMockStore from 'redux-mock-store';
 import thunk from 'redux-thunk';
 import renderer from 'react-test-renderer';
 import AppTutorialScreen from '../AppTutorialScreen';
-import AccountScreen from '../AccountScreen';
 import {initialState} from '../../config/jest/mockStore';
 
 let store;
 const middlewares = [thunk];
 const mockStore = configureMockStore(middlewares);
 
-let findElement = function(tree,element) {
-    console.warn(tree);
-    let result = undefined;
-    for (let node in tree.children){
-        // if(tree.children[node].props.testID == element){
-            result = true;
-        // }
-    }
-    return result;
-}
+
 describe('AppTutorial', () => {
     let wrapper;
     beforeEach(() => {
@@ -31,18 +22,24 @@ describe('AppTutorial', () => {
     it('snapshot',()=> {
         expect(wrapper).toMatchSnapshot();
     })
-});
-test('Find element', () => {
-    const AppData = renderer.create(<AppTutorialScreen />).toJSON();
-    expect(findElement(AppData,'username')).toBeDefined();
-});
-describe('AccountScreen', () => {
-    let wrapper;
-    beforeEach(() => {
-        store = mockStore(initialState);
-        wrapper = shallow(<AccountScreen store={store} />).dive();
-    })
-    it('snapshot',()=> {
-        expect(wrapper).toMatchSnapshot();
-    })
+    it('function and state testing', () => {
+        const AppTutorialData = renderer.create(<AppTutorialScreen />).getInstance();
+        AppTutorialData.changeState('React');
+        expect(AppTutorialData.state.person).toEqual('React');
+    });
+
+    /// For state testing  ---- > component.instance().state
+    /// For finding button and simulating component.dive().find({ testID: 'signUp' }).first().simulate('press');
+
+    it('Click events tesing ', () => {
+        const mockFunc = jest.fn();
+        const navigation = {
+            navigate: mockFunc,
+            goBack: mockFunc
+        }
+        const component = shallow(<AppTutorialScreen onPress={mockFunc} navigation={navigation} />);
+        console.log(component.instance().state)
+        component.dive().find(TouchableOpacity).first().simulate('press');
+        expect(mockFunc).toHaveBeenCalled();
+    });
 });
